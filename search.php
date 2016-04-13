40d027a85cbce1fa42bc21d59d82a24db69ca2a9
José Barrachina <jbarrachina@ausiasmarch.net>
2016-04-13 13:22:47 +0200

    estado y enviar correo de incidencia

--- search.php
+++ search.php
@@ -19,16 +19,16 @@
 
 // incluir la conexión a la base de datos
             include 'conexion.php';
-            $codigo = isset($_GET['code']) ? $_GET['code'] : "657880";//un parámetro
-
+            $codigo = isset($_GET['code']) ? $_GET['code'] : "657880"; //un parámetro
 // Elegir los datos que deseamos recuperar de la tabla
             $query = <<<SQL
-       SELECT com.name, serial, otherserial, glpi_plugin_fusioninventory_inventorycomputercomputers.last_fusioninventory_update, glpi_locations.completename, glpi_computermodels.name 
-           FROM glpi_computers com 
-           LEFT JOIN glpi_plugin_fusioninventory_inventorycomputercomputers ON glpi_plugin_fusioninventory_inventorycomputercomputers.computers_id = com.id 
-           LEFT JOIN glpi_locations ON com.locations_id = glpi_locations.id 
-           LEFT JOIN glpi_computermodels ON com.computermodels_id = glpi_computermodels.id  
-           WHERE otherserial = ? AND is_template = 0 AND is_deleted = 0
+       SELECT com.name, SERIAL, otherserial, glpi_plugin_fusioninventory_inventorycomputercomputers.last_fusioninventory_update, glpi_locations.completename, glpi_computermodels.name, glpi_states.completename
+            FROM glpi_computers com
+            LEFT JOIN glpi_plugin_fusioninventory_inventorycomputercomputers ON glpi_plugin_fusioninventory_inventorycomputercomputers.computers_id = com.id
+            LEFT JOIN glpi_locations ON com.locations_id = glpi_locations.id
+            LEFT JOIN glpi_computermodels ON com.computermodels_id = glpi_computermodels.id
+            LEFT JOIN glpi_states ON com.states_id = glpi_states.id
+            WHERE otherserial =  ? AND is_template =0 AND is_deleted = 0
 SQL;
 //echo $query;
 
@@ -38,30 +38,36 @@ SQL;
                     die('Error de ejecución de la consulta. ' . $conexion->error);
                 }
 // recoger los datos
-                $stmt->bind_result($name, $serial, $otherserial, $fusion_update, $location, $model);
+                $stmt->bind_result($name, $serial, $otherserial, $fusion_update, $location, $model, $state);
 
 
 //recorrido por el resultado de la consulta
                 while ($stmt->fetch()) {
                     echo "<div class='row'>";
-                    echo "<div class=\"col-md-3\">";                       
-                        echo "<h2 class=\"bg-primary\">$name</h2></div>";
+                    echo "<div class=\"col-md-3\">";
+                    echo "<h2 class=\"bg-primary\">$name</h2></div>";
                     echo "<div class=\"col-md-4\">";
                     echo "<h4>Modelo: $model</h4>";
                     echo "<h4>Nº Serie: $serial</h4>";
                     echo "<h5>Nº Inventario: $otherserial</h5></div>";
                     echo "<div class='col-md-4'>Última actualización: $fusion_update";
-                    echo "<p>Ubicación: $location</p></div>";
+                    echo "<p>Ubicación: $location</p>";
+                    echo "<p>Estado: $state</p></div>";
                     echo "</div>";
                 }
                 // end table
-
 
                 $stmt->close();
             } else {
                 die('Imposible preparar la consulta. ' . $conexion->error);
             }
             ?>
+            <p>
+                incidencias a:
+                <a href="mailto:moodle@ausiasmarch.net?Subject=Incidencia: <?= $name.": ".$serial ?>" target="_top">Enviar ... </a>
+            </p>
+
+
         </div>
     </body>
 </html>
