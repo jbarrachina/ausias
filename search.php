c88f3c53c3ec252f06919ec0487bcb67ee4560eb
jose <jose@jose-Lenovo-G50-70>
2016-04-28 20:14:07 +0200

    incidencias añadidas

--- search.php
+++ search.php
@@ -11,15 +11,27 @@
     <body>
         <div class="container">
             <?php
-            /*             * *********************************************************
+            /*             * ********************************************************
              * script search.php 
              * necesita un parámetro code
              * utiliza la base de datos glpi
              * ********************************************************* */
 
+            function getStatus($status) {
+                switch ($status) {
+                    case 1: return "Sin Asignar";
+                    case 2: return "Asignada";
+                    case 3: return "Planificada";
+                    case 4: return "En Espera";
+                    case 5: return "Resuelta";
+                    case 6: return "Cerrada";
+                    default: return "Status Incorrecto";
+                }
+            }
+
 // incluir la conexión a la base de datos
             include 'conexion.php';
-            $codigo = isset($_GET['code']) ? $_GET['code'] : "432932"; //un parámetro
+            $codigo = isset($_GET['code']) ? $_GET['code'] : "368939"; //un parámetro
 // Elegir los datos que deseamos recuperar de la tabla
             $query = <<<SQL
        SELECT com.id, com.name, com.SERIAL, otherserial, glpi_plugin_fusioninventory_inventorycomputercomputers.last_fusioninventory_update, glpi_locations.completename, glpi_computermodels.name, glpi_states.completename, glpi_items_devicememories.size
@@ -46,7 +58,7 @@ SQL;
                 while ($stmt->fetch()) {
                     echo "<div class='row'>";
                     echo "<div class=\"col-md-3\">";
-                    echo "<h2 class=\"bg-primary\">$pcid  -  $name</h2></div>";
+                    echo "<h2 class=\"bg-primary\">$name</h2></div>";
                     echo "<div class=\"col-md-4\">";
                     echo "<h4>Modelo: $model</h4>";
                     echo "<h4>Nº Serie: $serial</h4>";
@@ -67,51 +79,61 @@ SQL;
                 die('Imposible preparar la consulta. ' . $conexion->error);
             }
 
+            $incidencia = false;
+
             $query = <<<SQL
-                SELECT status, date, glpi_tickets.name, closedate
+                SELECT status, date, glpi_tickets.name, closedate, content, solution, concat(firstname, ' ', realname)
                 FROM glpi_items_tickets
-                LEFT JOIN glpi_tickets ON tickets_id = glpi_tickets.id
-                WHERE glpi_items_tickets.items_id = ? AND is_deleted = 0
+                LEFT JOIN glpi_tickets ON glpi_items_tickets.tickets_id = glpi_tickets.id
+                LEFT JOIN glpi_tickets_users ON glpi_items_tickets.tickets_id = glpi_tickets_users.tickets_id
+                LEFT JOIN glpi_users ON glpi_users.id = glpi_tickets_users.users_id    
+                WHERE glpi_items_tickets.items_id = ? AND glpi_tickets.is_deleted = 0 AND glpi_tickets_users.type = 2
 SQL;
-                    
+
             if ($stmt = $conexion->prepare($query)) {
                 $stmt->bind_param('s', $pcid);
                 if (!$stmt->execute()) {
                     die('Error de ejecución de la consulta. ' . $conexion->error);
                 }
 // recoger los datos
-                $stmt->bind_result($status, $date, $name,  $closedate);
+                $stmt->store_result();
+                $stmt->bind_result($status, $date, $name, $closedate, $content, $solution, $tecnician);
 
 
 //recorrido por el resultado de la consulta
                 while ($stmt->fetch()) {
                     echo "<div class='row'>";
                     echo "<div class=\"col-md-2\">";
-                    echo "<h4 class=\"bg-primary\">$status</h4></div>";
-                    echo "<div class=\"col-md-8\">";
-                    echo "<h4>Entrada: $date</h4>";
-                    echo "<h3>$name</h3>";
-                    echo "<h5></h5></div>";
-                    echo "<div class='col-md-2'>Cierre: $closedate </div>";
+                    echo "<h4 class=\"bg-primary\">" . getStatus($status) . "</h4>";
+                    echo "<h4 class=\"bg-info\">" . $tecnician . "</h4>";
                     echo "</div>";
+                    echo "<div class=\"col-md-8\">";
+                    echo "<h5>Abierta: $date - Cerrada: $closedate </h5>";
+                    echo "<h4>$name</h4>";
+                    echo "<div>{$content}</div>";
+                    echo "<div class=\"bg-success\">".htmlspecialchars_decode($solution)."</div>";
+                    echo "</div>";
+                    echo "</div>";
+                    if ($status < 6) { //si hay incidencia sin cerrar
+                        $incidencia = true;
+                    }
                 }
                 // end table
 
                 $stmt->close();
             } else {
                 die('Imposible preparar la consulta. ' . $conexion->error);
-            }        
-                    
+            }
+
+            if (!$incidencia) {
+                ?>
+                <p>
+                    incidencias a:
+                    <a href="mailto:moodle@ausiasmarch.net?Subject=Incidencia: <?= $name . ": " . $serial ?>" target="_top">Enviar ... </a>
+                </p>
+                <?php
+            }
             ?>
-
-
-
-            <p>
-                incidencias a:
-                <a href="mailto:moodle@ausiasmarch.net?Subject=Incidencia: <?= $name . ": " . $serial ?>" target="_top">Enviar ... </a>
-            </p>
-
-
         </div>
     </body>
 </html>
